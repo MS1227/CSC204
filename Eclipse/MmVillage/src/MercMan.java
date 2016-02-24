15bf4598d5ad7f77eeadc313521e218020062724
Matt Schnider <matt.schnider@gmail.com>
2016-02-24 09:52:28 -0500

    greeps....

--- Eclipse/MmVillage/src/MercMan.java
+++ Eclipse/MmVillage/src/MercMan.java
@@ -1,8 +1,6 @@
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.GradientPaint;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.Stroke;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
