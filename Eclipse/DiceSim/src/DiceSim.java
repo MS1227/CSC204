25aba630d540f701b8fb08627b5a3ea698d1e8ce
Matt Schnider <matt.schnider@gmail.com>
2016-04-04 09:49:41 -0400

    Started Dice assignment

---
+++ Eclipse/DiceSim/src/DiceSim.java
@@ -0,0 +1,18 @@
+
+public class DiceSim 
+{
+private int sidesOfDice;
+private int numOfDice;
+private int numOfRolls;
+int [] countOfRolls;
+int [] theDice;
+
+public DiceSim()
+{
+	sidesOfDice = 6;
+	numOfDice = 1;
+	numOfRolls = 100;
+	
+}
+
+}
