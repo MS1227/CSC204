ad09902b5eeb6259bce948c764997706150246a7
Matt Schnider <matt.schnider@gmail.com>
2016-08-26 11:22:25 -0400

    practice

--- Eclipse/SummerHomework/src/SummerMain.java
+++ Eclipse/SummerHomework/src/SummerMain.java
@@ -4,16 +4,25 @@ public class SummerMain {
 
 	public static void main(String[] args)
 	{
-	 Scanner in = new Scanner(System.in);
+	 Scanner in = new Scanner(System.in); //Scanner for reading in data from the user.
+	 System.out.print("Please type a word or sentence: ");
+	 String word = new String(in.nextLine()); //The object, word, is instantiated and initialized with the user input.
 	 
-	 System.out.print("Please type a word: ");
-	 String word = new String(in.next());
+        System.out.println(word);
+	
+		
 	 
-	 Stringy test = new Stringy(word);
+	 Stringy test = new Stringy(word); //A new stringy object, test, is instantiated and passed the object, word.
+	 System.out.println();
 	 
 	 
-	test.revString();
-	test.countVowels();
+	test.revString(); //revString method call.
+	test.countVowels(); //countVowels method call.
+	test.isPalindrome(); //isPalindrome method call.
+	test.wordCount();
+	
+	System.out.println();
+	System.out.println("- word test ended successfully - "); // Message to tell the user all testing is complete.
 	
 	 
 		
